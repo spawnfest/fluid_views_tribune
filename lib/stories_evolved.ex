5784e0211dfb16cbfec2eff2fc4cf9c8c7387743
James Edward Gray II <james@graysoftinc.com>
2017-12-09 08:44:07 -0600

    mix new --sup

---
+++ lib/stories_evolved.ex
@@ -0,0 +1,18 @@
+defmodule StoriesEvolved do
+  @moduledoc """
+  Documentation for StoriesEvolved.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StoriesEvolved.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stories_evolved_test.exs
@@ -0,0 +1,8 @@
+defmodule StoriesEvolvedTest do
+  use ExUnit.Case
+  doctest StoriesEvolved
+
+  test "greets the world" do
+    assert StoriesEvolved.hello() == :world
+  end
+end
